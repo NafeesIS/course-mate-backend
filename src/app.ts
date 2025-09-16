f433eb1b03abea75088567ed521bfc854bbcff55
NafeesIS <nafeesshawon555@gmail.com>
2025-09-17 01:17:42 +0600

    moved server into app ts file

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ import globalErrorHandler from "./app/middlewares/globalErrorHandlers";
 import notFound from "./app/middlewares/notFound";
 import mongoose from "mongoose";
 import config from "./app/config";
-
+import { Server } from "http";
 const app: Application = express();
 
 // CORS configuration
@@ -38,10 +38,10 @@ app.use(express.json());
 const healthCheck = (req: Request, res: Response) => {
   res.status(200).send({
     success: true,
-    message: 'Application is up and running.',
+    message: "Application is up and running.",
   });
 };
-app.get('/', healthCheck);
+app.get("/", healthCheck);
 // Static file serving for uploads
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 // SuperTokens middleware
@@ -55,4 +55,35 @@ app.use(notFound);
 app.use(supertokensErrorHandler());
 app.use(globalErrorHandler);
 
-export default app;
+let server: Server;
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string, {
+      serverSelectionTimeoutMS: 30000, // Increase timeout
+      socketTimeoutMS: 45000,
+      family: 4, // Force IPv4
+    });
+    console.log("✅ Connected to the database");
+
+    server = app.listen(config.port, () => {
+      console.log(`🚀 Course Mate server listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.error("❌ DB connection error", err);
+    process.exit(1);
+  }
+}
+
+main();
+
+// Graceful shutdown
+process.on("unhandledRejection", (err) => {
+  console.error("😈 Unhandled rejection", err);
+  server?.close(() => process.exit(1));
+});
+
+process.on("uncaughtException", (err) => {
+  console.error("😈 Uncaught exception", err);
+  process.exit(1);
+});
